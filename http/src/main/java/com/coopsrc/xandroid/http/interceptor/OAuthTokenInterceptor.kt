28fe2f734923916d2e9687440235f37e3b72f367
Zhang Tingkuo <zhangtingkuo@gmail.com>
2019-10-23 14:18:36 +0800

    Fix OAuthTokenInterceptor#intercept
    Type: bugfix
    Scope: http
    
    Signed-off-by: Zhang Tingkuo <zhangtingkuo@gmail.com>

--- http/src/main/java/com/coopsrc/xandroid/http/interceptor/OAuthTokenInterceptor.kt
+++ http/src/main/java/com/coopsrc/xandroid/http/interceptor/OAuthTokenInterceptor.kt
@@ -4,9 +4,11 @@ import com.coopsrc.xandroid.http.config.HttpConstants
 import com.coopsrc.xandroid.utils.LogUtils
 import okhttp3.Interceptor
 import okhttp3.Response
+import java.io.IOException
 
 abstract class OAuthTokenInterceptor : Interceptor {
 
+    @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         LogUtils.d("intercept: chain=%s", chain)
 

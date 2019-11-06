241b8e911c8f4422ef65df1d4384fc7035473f81
Zhang Tingkuo <zhangtingkuo@gmail.com>
2019-11-06 19:38:02 +0800

    Update apiproxy.
    Signed-off-by: Zhang Tingkuo <zhangtingkuo@gmail.com>

--- downloader/src/main/java/com/coopsrc/xandroid/downloader/api/DownloadApiProxy.kt
+++ downloader/src/main/java/com/coopsrc/xandroid/downloader/api/DownloadApiProxy.kt
@@ -1,14 +1,37 @@
 package com.coopsrc.xandroid.downloader.api
 
+import android.content.Context
+import com.coopsrc.xandroid.downloader.ExDownloader
 import com.coopsrc.xandroid.downloader.utils.Logger
 import com.coopsrc.xandroid.http.api.BaseApiProxy
+import com.coopsrc.xandroid.http.config.HttpClientConfig
 import io.reactivex.Maybe
 import okhttp3.ResponseBody
+import retrofit2.CallAdapter
 import retrofit2.Response
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 
-internal object DownloadApiProxy : BaseApiProxy<DownloadApiService,  DownloadClientConfig>() {
+internal object DownloadApiProxy : BaseApiProxy<DownloadApiService >() {
 
     private const val tag = "DownloadApiImpl"
+
+    private class DownloadClientConfig : HttpClientConfig() {
+        override fun getAppContext(): Context? {
+            return ExDownloader.downloadCore.config.context
+        }
+
+        override fun callAdapterFactories(): LinkedHashSet<CallAdapter.Factory> {
+            return linkedSetOf(RxJava2CallAdapterFactory.create())
+        }
+
+        override fun useDebugMonitor(): Boolean {
+            return ExDownloader.withDebug()
+        }
+    }
+
+    override fun clientConfig(): HttpClientConfig {
+        return DownloadClientConfig()
+    }
 
     fun detect(url: String, range: String = "bytes=0-0"): Maybe<Response<Void>> {
         Logger.i(tag, "detect: url=>$url")
--- http/src/main/java/com/coopsrc/xandroid/http/api/BaseApiProxy.kt
+++ http/src/main/java/com/coopsrc/xandroid/http/api/BaseApiProxy.kt
@@ -12,26 +12,24 @@ import java.lang.reflect.ParameterizedType
  *
  * Date: 2019-09-02 14:08
  */
-abstract class BaseApiProxy<T, C : HttpClientConfig> protected constructor() {
+abstract class BaseApiProxy<T> protected constructor() {
 
     private var retrofit: Retrofit
     protected var apiService: T
 
     init {
+        retrofit = RetrofitManager.newRetrofit(clientConfig())
+
         val parameterizedType = javaClass.genericSuperclass as ParameterizedType
-
-        val config: Class<C> = parameterizedType.actualTypeArguments[1] as Class<C>
-        retrofit = RetrofitManager.newRetrofit(config.newInstance())
-
         val service: Class<T> = parameterizedType.actualTypeArguments[0] as Class<T>
         apiService = createApiService(service)
     }
 
-    private fun createApiService(service: Class<T>): T {
-        return retrofit.create(service)
+    protected open fun clientConfig(): HttpClientConfig {
+        return BaseApiClientConfig()
     }
 
-    fun <S> create(service: Class<S>): S {
+    private fun createApiService(service: Class<T>): T {
         return retrofit.create(service)
     }
 
@@ -39,12 +37,10 @@ abstract class BaseApiProxy<T, C : HttpClientConfig> protected constructor() {
         return retrofit.newBuilder().baseUrl(baseUrl).build().create(service)
     }
 
-    fun <S> create(baseUrl: String, service: Class<S>): S {
-        return retrofit.newBuilder().baseUrl(baseUrl).build().create(service)
-    }
-
     fun updateBaseUrl(baseUrl: String) {
         retrofit = retrofit.newBuilder().baseUrl(baseUrl).build()
     }
+
+    protected open class BaseApiClientConfig : HttpClientConfig()
 
 }

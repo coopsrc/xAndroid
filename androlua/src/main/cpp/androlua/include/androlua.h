366ac506694ae8fb552221d51d7e038108dcf1a5
Zhang Tingkuo <zhangtingkuo@gmail.com>
2019-12-25 19:25:45 +0800

    update register native methods for androlua.
    Signed-off-by: Zhang Tingkuo <zhangtingkuo@gmail.com>

--- androlua/src/main/cpp/androlua/include/androlua.h
+++ androlua/src/main/cpp/androlua/include/androlua.h
@@ -2,27 +2,22 @@
 // Created by tingkuo on 19-11-23.
 //
 
+#include <jni.h>
+
 #ifndef XANDROID_ANDROLUA_H
 #define XANDROID_ANDROLUA_H
 
-#include <jni.h>
-#include "logger.h"
-
+#ifdef __cplusplus
 extern "C" {
+#endif
 
-#include "lua.hpp"
-#include "luaconf.h"
+JNIEXPORT jstring JNICALL getLuaVersion(JNIEnv *env, jclass clazz);
+JNIEXPORT jstring JNICALL getLuaRelease(JNIEnv *env, jclass clazz);
+JNIEXPORT jstring JNICALL getLuaCopyright(JNIEnv *env, jclass clazz);
+JNIEXPORT jstring JNICALL getLuaAuthors(JNIEnv *env, jclass clazz);
 
-
-JNIEXPORT jstring JNICALL
-Java_com_coopsrc_xandroid_androlua_Androlua_getLuaVersion(JNIEnv *env, jclass clazz);
-JNIEXPORT jstring JNICALL
-Java_com_coopsrc_xandroid_androlua_Androlua_getLuaRelease(JNIEnv *env, jclass clazz);
-JNIEXPORT jstring JNICALL
-Java_com_coopsrc_xandroid_androlua_Androlua_getLuaCopyright(JNIEnv *env, jclass clazz);
-JNIEXPORT jstring JNICALL
-Java_com_coopsrc_xandroid_androlua_Androlua_getLuaAuthors(JNIEnv *env, jclass clazz);
-
-};
+#ifdef __cplusplus
+}
+#endif
 
 #endif //XANDROID_ANDROLUA_H
--- androlua/src/main/cpp/androlua/src/androlua.cpp
+++ androlua/src/main/cpp/androlua/src/androlua.cpp
@@ -5,10 +5,41 @@
 #include "androlua.h"
 #include <string>
 
-extern "C" {
+#include "logger.h"
 
-JNIEXPORT jstring JNICALL
-Java_com_coopsrc_xandroid_androlua_Androlua_getLuaVersion(JNIEnv *env, jclass clazz) {
+#include "lua.hpp"
+#include "luaconf.h"
+
+JNINativeMethod methods[] = {
+        {"_getLuaVersion",   "()Ljava/lang/String;", (void *) getLuaVersion},
+        {"_getLuaRelease",   "()Ljava/lang/String;", (void *) getLuaRelease},
+        {"_getLuaCopyright", "()Ljava/lang/String;", (void *) getLuaCopyright},
+        {"_getLuaAuthors",   "()Ljava/lang/String;", (void *) getLuaAuthors}
+};
+
+jint registerNativeMethod(JNIEnv *env) {
+    jclass clazz = env->FindClass("com/coopsrc/xandroid/androlua/Androlua");
+    if (env->RegisterNatives(clazz, methods, sizeof(methods) / sizeof(methods[0])) < 0) {
+        return JNI_ERR;
+    }
+
+    return JNI_OK;
+}
+
+jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    JNIEnv *env = nullptr;
+    if (vm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6) != JNI_OK) {
+        return JNI_ERR;
+    }
+
+    if (registerNativeMethod(env) != JNI_OK) {
+        return JNI_ERR;
+    }
+
+    return JNI_VERSION_1_6;
+}
+
+JNIEXPORT jstring JNICALL getLuaVersion(JNIEnv *env, jclass clazz) {
     std::string lua_version = LUA_VERSION;
 
     ALOGE("lua version: %s", lua_version.c_str());
@@ -16,8 +47,7 @@ Java_com_coopsrc_xandroid_androlua_Androlua_getLuaVersion(JNIEnv *env, jclass cl
     return env->NewStringUTF(lua_version.c_str());
 }
 
-JNIEXPORT jstring JNICALL
-Java_com_coopsrc_xandroid_androlua_Androlua_getLuaRelease(JNIEnv *env, jclass clazz) {
+JNIEXPORT jstring JNICALL getLuaRelease(JNIEnv *env, jclass clazz) {
     std::string lua_release = LUA_RELEASE;
 
     ALOGE("lua release: %s", lua_release.c_str());
@@ -25,8 +55,7 @@ Java_com_coopsrc_xandroid_androlua_Androlua_getLuaRelease(JNIEnv *env, jclass cl
     return env->NewStringUTF(lua_release.c_str());
 }
 
-JNIEXPORT jstring JNICALL
-Java_com_coopsrc_xandroid_androlua_Androlua_getLuaCopyright(JNIEnv *env, jclass clazz) {
+JNIEXPORT jstring JNICALL getLuaCopyright(JNIEnv *env, jclass clazz) {
     std::string lua_copyright = LUA_COPYRIGHT;
 
     ALOGE("lua copyright: %s", lua_copyright.c_str());
@@ -34,13 +63,10 @@ Java_com_coopsrc_xandroid_androlua_Androlua_getLuaCopyright(JNIEnv *env, jclass 
     return env->NewStringUTF(lua_copyright.c_str());
 }
 
-JNIEXPORT jstring JNICALL
-Java_com_coopsrc_xandroid_androlua_Androlua_getLuaAuthors(JNIEnv *env, jclass clazz) {
+JNIEXPORT jstring JNICALL getLuaAuthors(JNIEnv *env, jclass clazz) {
     std::string lua_authors = LUA_AUTHORS;
 
     ALOGE("lua authors: %s", lua_authors.c_str());
 
     return env->NewStringUTF(lua_authors.c_str());
-}
-
 }

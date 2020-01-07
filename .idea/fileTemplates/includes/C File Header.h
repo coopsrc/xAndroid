70a38d254b70acfee9060378c1c50669fd48a846
Zhang Tingkuo <zhangtingkuo@gmail.com>
2020-01-07 16:59:20 +0800

    config[all]: Update C File Header
    	- update c file header.
    	- update commit template.
    
    Type: config
    Scope: all
    Signed-off-by: Zhang Tingkuo <zhangtingkuo@gmail.com>

--- .idea/fileTemplates/includes/C File Header.h
+++ .idea/fileTemplates/includes/C File Header.h
@@ -1,7 +1,7 @@
 #parse("License.txt")
 #if ($HEADER_COMMENTS)
 //
-// Created by $USER_NAME on ${DATE}.
+// Created by $USER_NAME on ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}.
 #if ($ORGANIZATION_NAME && $ORGANIZATION_NAME != "")
 // Copyright (c) $YEAR ${ORGANIZATION_NAME}#if (!$ORGANIZATION_NAME.endsWith(".")).#end All rights reserved.
 #end

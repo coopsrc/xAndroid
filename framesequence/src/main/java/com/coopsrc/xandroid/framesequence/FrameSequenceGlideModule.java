8b23d1559121402343911b05c48821fd44a7c1c8
Zhang Tingkuo <zhangtingkuo@gmail.com>
2020-01-11 14:32:30 +0800

    feature[feamesequence]: update glide version
        - update glide vertion for framesequence to 4.11.0
    
    Type: feature
    Scope: sequence
    
    Signed-off-by: Zhang Tingkuo <zhangtingkuo@gmail.com>

--- framesequence/src/main/java/com/coopsrc/xandroid/framesequence/FrameSequenceGlideModule.java
+++ framesequence/src/main/java/com/coopsrc/xandroid/framesequence/FrameSequenceGlideModule.java
@@ -23,10 +23,12 @@ import androidx.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
 
 import java.io.InputStream;
 
+@GlideModule
 public class FrameSequenceGlideModule extends AppGlideModule {
     @Override
     public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
